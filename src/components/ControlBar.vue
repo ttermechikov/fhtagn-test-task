1304360de8e15b06f646fbbb0029aad07b8abab1
Temirlan <temirlan.termechikov@gmail.com>
2021-04-11 12:14:26 +0600

    Added the 'ControlBar' component

---
+++ src/components/ControlBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <b-container>
+    <b-row class="control-bar-row">
+      <b-col
+        >Всего: <strong>{{ selected }}</strong>
+      </b-col>
+      <b-col>
+        <b-row class="control-bar-row">
+          <b-col>Количество:</b-col>
+          <b-col
+            ><b-form-select
+              v-model="selected"
+              v-on:change="updatePostsCount"
+              :options="options"
+              size="sm"
+            ></b-form-select
+          ></b-col>
+        </b-row>
+      </b-col>
+      <b-col>
+        <b-button variant="outline-primary">Экспорт</b-button>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'ControlBar',
+  data() {
+    return {
+      selected: this.postsPerPage,
+      options: [
+        { value: 10, text: '10' },
+        { value: 25, text: '25' },
+        { value: 50, text: '50' },
+      ],
+    };
+  },
+  props: {
+    postsPerPage: Number,
+    setPostsPerPage: Function,
+  },
+  methods: {
+    updatePostsCount() {
+      this.$emit('setPostsPerPage', this.selected);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.control-bar-row {
+  align-items: center;
+}
+</style>
