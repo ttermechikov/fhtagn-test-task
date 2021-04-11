b7db1c09d0e1167e70da317f111cc5b670afd9ae
Temirlan <temirlan.termechikov@gmail.com>
2021-04-11 18:14:34 +0600

    Added the 'Posts' component

---
+++ src/components/Posts.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-container>
+    <div v-for="post in posts" :key="post.id" class="post">
+      <h2>{{ post.title }}</h2>
+      <i class="text-muted">by @{{ getUsernameById(post.userId) }}</i>
+      <p>{{ post.body }}</p>
+    </div>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'Posts',
+  props: {
+    posts: Array,
+    users: Array,
+  },
+  methods: {
+    getUsernameById(id) {
+      const user = this.users.find((u) => u.id === id);
+      return user && user.username;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.post {
+  text-align: left;
+  margin-bottom: 1.5rem;
+}
+</style>
